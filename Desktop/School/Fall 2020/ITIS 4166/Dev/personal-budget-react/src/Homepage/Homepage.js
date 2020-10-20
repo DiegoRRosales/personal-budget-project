b6030ca5e03b0f844917ef523fcdbe86b37d3684
DiegoRRosales <diego_raphael_98@yahoo.com>
2020-10-19 23:50:52 -0400

    coding exercise 3 changes added

--- Desktop/School/Fall 2020/ITIS 4166/Dev/personal-budget-react/src/Homepage/Homepage.js
+++ Desktop/School/Fall 2020/ITIS 4166/Dev/personal-budget-react/src/Homepage/Homepage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Chart from 'chart.js';
+import axios from 'axios';
 
 function Homepage() {
   return (
@@ -73,7 +76,48 @@ function Homepage() {
         </div>
 
     </main>
-  );
+  )
+
+  var dataSource = {
+    datasets: [
+        {
+            data: [],
+            backgroundColor: [
+                '#ffcd56',
+                '#ff6384',
+                '#36a2eb',
+                '#fd6b19',
+                '#123456',
+                '#22cca7',
+                '#765432',
+            ],
+        }
+    ],
+    labels: []
+  };
+
+function createChart() {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myPieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: dataSource
+    });
+}
+
+function getBudget() {
+    axios.get('http://localhost:3000/budget')
+    .then(function (res) {
+        console.log(res);
+        for (var i = 0; i < res.data.myBudget.length; i++) {
+            dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
+            dataSource.labels[i] = res.data.myBudget[i].title;
+        }
+        createChart();
+    });
+
+}
+
+getBudget();
 }
 
 export default Homepage;
